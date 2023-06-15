6f31799e4601fb8b9aa5c54df1b34c46677ad84c
Salvador <salvadorebrandolin@mi.unc.edu.ar>
2023-06-14 23:11:06 -0300

    nb changes

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Install programs
+# Install package manager programs
 xargs sudo apt-get install -y < requirements.txt
 
 chsh -s $(which zsh)
+
+# Install kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 # Install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-
 
 # GitHub cloning
 git clone https://github.com/b3nj5m1n/xdg-ninja.git ~/.local/bin/xdg-ninja
