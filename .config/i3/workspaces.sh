f2f0be0107b0784f653876d7438cfa0e33b523d7
salvador <salvadorebrandolin@mi.unc.edu.ar>
2023-07-12 00:08:09 -0300

    new workspaces

--- .config/i3/workspaces.sh
+++ .config/i3/workspaces.sh
@@ -3,10 +3,14 @@
 # List of most used workspaces
 # Open with mod+o
 
-workspaces="ReactorD"
+workspaces="ReactorD
+dotfiles
+dipypr"
 
 wp=$(echo "$workspaces" | dmenu -i -fn "Liberation Mono Bold")
 
 case "$wp" in
 	"ReactorD") code ~/code/ReactorD ;;
+	"dipypr") code ~/code/dipypr ;;
+	"dotfiles") code ~/dotfiles ;;
 esac
