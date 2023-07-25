72a736a728c6a19161673a97b8811fb66f804687
Salvador <salvadorebrandolin@mi.unc.edu.ar>
2023-07-25 12:38:56 -0300

    nuevos espacios de trabajo

--- .config/i3/workspaces.sh
+++ .config/i3/workspaces.sh
@@ -3,10 +3,17 @@
 # List of most used workspaces
 # Open with mod+o
 
-workspaces="ReactorD"
+workspaces="ReactorD
+dipypr
+dotfiles
+bioactividad2023
+"
 
 wp=$(echo "$workspaces" | dmenu -i -fn "Liberation Mono Bold")
 
 case "$wp" in
 	"ReactorD") code ~/code/ReactorD ;;
+	"dipypr") code ~/code/dipypr ;;
+	"dotfiles") code ~/code/dotfiles ;;
+	"bioactividad2023") code ~/code/bioactividad_2023 
 esac
