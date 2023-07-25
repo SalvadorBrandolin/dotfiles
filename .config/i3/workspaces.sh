e24d291e19e3b8eafab7b2f61d44fcf18d8cac80
salvador <salvadorebrandolin@mi.unc.edu.ar>
2023-07-24 22:27:18 -0300

    Revert "new workspaces"
    This reverts commit f2f0be0107b0784f653876d7438cfa0e33b523d7.

--- .config/i3/workspaces.sh
+++ .config/i3/workspaces.sh
@@ -3,14 +3,10 @@
 # List of most used workspaces
 # Open with mod+o
 
-workspaces="ReactorD
-dotfiles
-dipypr"
+workspaces="ReactorD"
 
 wp=$(echo "$workspaces" | dmenu -i -fn "Liberation Mono Bold")
 
 case "$wp" in
 	"ReactorD") code ~/code/ReactorD ;;
-	"dipypr") code ~/code/dipypr ;;
-	"dotfiles") code ~/dotfiles ;;
 esac
