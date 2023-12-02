364c73560e054acd4f6c8a0fed98e5e54f53f52d
salvador <salvadorebrandolin@mi.unc.edu.ar>
2023-12-02 13:50:00 -0300

    more programs

--- setup.sh
+++ setup.sh
@@ -27,20 +27,15 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugi
 # =============================================================================
 # Not apt packages
 # =============================================================================
-# Install jill.sh
+# Install jill.sh (Julia -- TODO: need to change to juliaup)
 cd $HOME/.local/bin && curl -fsSL https://raw.githubusercontent.com/abelsiqueira/jill/main/jill.sh > jill.sh && cd -
 
 # Install kitty
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 # flatpak packages
-flatpak install avogadro
-
-
-# =============================================================================
-# Python setup
-# =============================================================================
-pip install virtualenv virtualenvwrapper
+flatpak run com.stremio.Stremio
+flatpak run org.libretro.RetroArch
 
 
 # =============================================================================
@@ -51,5 +46,10 @@ git clone https://github.com/b3nj5m1n/xdg-ninja.git ~/.local/bin/xdg-ninja
 mkdir ~/code
 
 git clone git@github.com:SalvadorBrandolin/ugropy.git ~/code/ugropy
-
 git clone git@github.com:SalvadorBrandolin/dipypr.git ~/code/dipypr
+
+
+# =============================================================================
+# Python setup
+# =============================================================================
+pip install virtualenv virtualenvwrapper
