69e97d0fac5ec0e5712bbe9f53e8fa16c20528b1
salvador <salvadorebrandolin@mi.unc.edu.ar>
2024-03-28 12:08:34 -0300

    julia revise

---
+++ .local/share/julia/config/startup_ijulia.jl
@@ -0,0 +1,8 @@
+# =============================================================================
+# Revise load on start
+# =============================================================================
+try
+    @eval using Revise
+catch e
+    @warn "Error initializing Revise" exception=(e, catch_backtrace())
+end
