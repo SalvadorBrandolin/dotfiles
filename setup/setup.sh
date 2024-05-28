9751cbba5f6ea4de41841a5d2eab3c2b33148950
salvador <salvadorebrandolin@mi.unc.edu.ar>
2024-05-28 19:54:06 -0300

    ...

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
 
-bash ~/dotfiles/1-stow.sh
+bash ~/dotfiles/setup/1-stow.sh
 
 # .profile
 source ~/.profile
 
-sudo bash ~/dotfiles/2-root_dependencies.sh
-bash ~/dotfiles/3-no_root_dependencies.sh
-bash ~/4-final_confs.sh
+sudo bash ~/dotfiles/setup/2-root_dependencies.sh
+bash ~/dotfiles/setup/3-no_root_dependencies.sh
+bash ~/setup/4-final_confs.sh
